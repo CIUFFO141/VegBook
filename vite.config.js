86b4ff5bf7d60370b3cd8328bb049d5e68364d57
Luca De Martini <luca-dema@live.it>
2024-02-29 18:11:26 +0100

    sistemazione module

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs/promises';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
 })
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import fs from 'fs/promises';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   esbuild: {
+//     loader: 'jsx',
+//   },
+//   resolve: {
+//     alias: {
+//       './runtimeConfig': './runtimeConfig.browser',
+//     },
+//   },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       loader: {
+//         '.js': 'jsx',
+//       },
+//     },
+//   },
+// })
